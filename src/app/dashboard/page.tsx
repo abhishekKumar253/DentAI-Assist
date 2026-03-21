978cc97c542171e107d19a4ee60b7383ae3d6b24
abhishekkumar4553 <abhishekgupta45591@gmail.com>
2026-03-22 01:28:11 +0530

    dashboard-page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainActions from "@/components/dashboard/MainActions";
 import Navbar from "@/components/Navbar";
-
+import WelcomeSection from "@/components/voice/WelcomeSection";
 
 function DashboardPage() {
   return (
-    <div>
+    <>
       <Navbar />
-      DASHBOARD
-    </div>
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
   );
 }
 
